b9955b5487991c15b473da5ee647b4be02bbf1a4
MSY19961022 <40051469+MSY19961022@users.noreply.github.com>
2018-09-16 10:39:40 +0800

    init

---
+++ springboot/src/main/java/top/msycandy/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package top.msycandy.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/main")
+    public String hello(){
+        return "Hello";
+    }
+}
